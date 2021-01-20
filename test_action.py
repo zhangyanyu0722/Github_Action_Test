a14ba79b0dc4998080dc9c39aa33894dd968d98f
YANYU ZHANG <zhangya@bu.edu>
2021-01-20 15:59:09 -0500

    Update test_action.py

--- test_action.py
+++ test_action.py
@@ -1,8 +1,13 @@
-from example import *
+from example import * # import functions from other files
 
 def test_add():
     assert add(2, 3) == 5
     assert add('boston', 'university') == 'bostonuniversity'
+
+
+def test_subtract():
+    assert subtract(2, 3) == -1
     
     
 test_add()
+test_subtract()
