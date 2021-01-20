5982a7fe3e88844c10f3a1239ab1702afbc40fb9
YANYU ZHANG <zhangya@bu.edu>
2021-01-20 15:56:40 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,16 +1,19 @@
+# ============================================================
+# Defining your own functions here
+# ============================================================
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
 
+# ============================================================
+# Defining your own testing here
+# ============================================================
 def test_add():
     assert add(2, 3) == 5
     assert add('boston', 'university') == 'bostonuniversity'
 
 
-def subtract(a, b):
-    return a + b  # <--- fix this in step 8
-
-
-# uncomment the following test in step 5
-#def test_subtract():
-#    assert subtract(2, 3) == -1
+def test_subtract():
+    assert subtract(2, 3) == -1
