1f02750cce042843345549878f19b3654b8dfd19
YANYU ZHANG <zhangya@bu.edu>
2021-01-20 16:06:34 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,15 +1,24 @@
+import numpy as np
 # ============================================================
 # Defining your own functions here
 # ============================================================
+
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
 
+def numpy_add(a, b):
+    return np.add(a, b)
+
+def numpy_subtract(a, b):
+    return np.subtract(a, b)
+
 # ============================================================
 # Defining your own testing here
 # ============================================================
+
 def test_add():
     assert add(2, 3) == 5
     assert add('boston', 'university') == 'bostonuniversity'
