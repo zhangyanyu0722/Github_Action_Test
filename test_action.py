7f910c28a424376eae0ae0206d26fc44db374c4f
YANYU ZHANG <zhangya@bu.edu>
2021-01-20 16:07:38 -0500

    Update test_action.py

--- test_action.py
+++ test_action.py
@@ -3,10 +3,12 @@ from example import * # import functions from other files
 def test_add():
     assert add(2, 3) == 5
     assert add('boston', 'university') == 'bostonuniversity'
+    assert numpy_add(1, 5) == 6
 
 
 def test_subtract():
     assert subtract(2, 3) == -1
+    assert numpy_subtract(0, 1) == -1
     
     
 test_add()
